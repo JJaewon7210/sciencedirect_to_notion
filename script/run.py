b6ac72ddda1524b6359c28700b953dda36fac086
Jaewon Jeoung <96426723+JJaewon7210@users.noreply.github.com>
2025-01-24 12:28:09 +0900

    Update run.py

--- script/run.py
+++ script/run.py
@@ -3,13 +3,11 @@ from gemini import main as gemini_main
 from notion import main as notion_main
 
 if __name__ == "__main__":
-    print(f"\n\n{"-"*50}")
+
     scrap_main()
     
-    print(f"\n\n{"-"*50}")
     gemini_main()
     
-    print(f"\n\n{"-"*50}")
     notion_main()
     
     print("All scripts have been executed")
